dd0018e73df0f4d807f6f6b4b4b525e2fefc2810
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hll2975@users.noreply.github.com>
2025-07-06 07:32:41 -0300

    Update models.py

--- backend/contents/models.py
+++ backend/contents/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 class Course(models.Model):
@@ -11,7 +10,13 @@ class Course(models.Model):
     image = models.ImageField(upload_to='courses/')
     level_required = models.CharField(max_length=10, choices=LEVEL_CHOICES, default='FREE')
 
+    def __str__(self):
+        return self.title
+
 class Pictogram(models.Model):
-    pictogram_id = models.CharField(max_length=100)
+    pictogram_id = models.CharField(max_length=100)  # ID desde la API de ARASAAC
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     caption = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.caption} ({self.pictogram_id})"
